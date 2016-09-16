73a496882360b22a0c36cda7580697249467a896
Taylan White <taylanwhite@gmail.com>
2016-09-16 13:00:29 -0600

    Added gson retrofit dependencies, as well as layout for front page deal. Errors on layout for front page deal

---
+++ app/src/main/java/com/example/taylanwhite/meh/activityWebTask.kt
@@ -0,0 +1,15 @@
+//package com.example.taylanwhite.meh
+//
+//import android.os.AsyncTask
+//import android.support.v7.app.AppCompatActivity
+//import android.os.Bundle
+//import android.util.Log
+//import java.io.BufferedReader
+//import java.net.HttpURLConnection
+//import java.net.URL
+//
+//class activityWebTask : AsyncTask<Void, Void, String>() {
+//
+//
+//
+//}
