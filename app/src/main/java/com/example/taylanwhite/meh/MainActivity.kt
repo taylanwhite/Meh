dcdd1c76ce7c7729f99ae5f89ceb4c5ff0e3e7f9
Taylan White <taylanwhite@gmail.com>
2016-09-22 14:22:37 -0600

    imageButton added to go through photos

--- app/src/main/java/com/example/taylanwhite/meh/MainActivity.kt
+++ app/src/main/java/com/example/taylanwhite/meh/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.taylanwhite.meh
 
 import android.content.ActivityNotFoundException
 import android.content.Intent
-import android.databinding.DataBindingUtil
 import android.graphics.Color
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
@@ -29,6 +28,7 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
+import java.io.File
 
 
 class MainActivity : AppCompatActivity() {
@@ -36,6 +36,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
 
         fetchData()
@@ -57,6 +58,8 @@ class MainActivity : AppCompatActivity() {
 
     fun fetchData()
     {
+        var i = 1
+
         MehService.retrofit.getDailyProduct().enqueue(object: Callback<DealObject>{
 
             override fun onResponse(call: Call<DealObject>?, response: Response<DealObject>?) {
@@ -69,9 +72,21 @@ class MainActivity : AppCompatActivity() {
                         txtName.text = response.deal.title
                         txtPrice.text = "Price:" + response.deal.items[0].price.toString()
                         txtDescription.text = response.deal.specifications
-                      //  imageDeal.setImageResource() =
-                       // response.deal.items[0].photo?.
-                        Picasso.with(this@MainActivity).load(response.deal.photos[0]).into(imageDeal)
+                        //imageDeal.setImageResource() =
+                       //response.deal.items[0].photo?.
+                       Picasso.with(this@MainActivity).load(response.deal.photos[0]).into(imageDeal)
+
+                        imageDeal.setOnClickListener {
+
+                            if (i >= response.deal.photos.size)
+                            {
+                                i = 0
+                            }
+                            Picasso.with(this@MainActivity).load(response.deal.photos[i]).into(imageDeal)
+
+                            i++
+
+                        }
 
 
                         btnBuy.setOnClickListener {
@@ -87,6 +102,17 @@ class MainActivity : AppCompatActivity() {
 
                         val currentLayout = findViewById(R.id.main_layout) as RelativeLayout
                         currentLayout.setBackgroundColor(Color.parseColor(response.deal.theme?.backgroundColor))
+
+
+//                        val mImageSwitcherPicasso = ImageSwitcherPicasso(this@MainActivity, imageSwitcher)
+//                        Picasso.with(this@MainActivity).load(response.deal.photos[0]).into(mImageSwitcherPicasso)
+//
+//                       // val image = Picasso.with(this@MainActivity).load(response.deal.photos[0])
+                        //val mImageSwitcherPicasso = ImageSwitcherPicasso(this@MainActivity, imageSwitcher)
+                        //
+                        //  imageSwitcher.setImageResource(image)
+
+
                     }
 
 
@@ -102,8 +128,16 @@ class MainActivity : AppCompatActivity() {
             }
 
 
+
+
+
+
         })
 
 
     }
+
+
+
+
 }
